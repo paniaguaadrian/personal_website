cce38e97aebccc2a415f0fc8dc694e67496f7f75
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2022-08-22 10:54:02 +0200

    Test the deploy flow with github actions and vercel deployment

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
             <Header title="BreadAndWater | Home" />
             <header>
                 <h1>BreadAndWater Website</h1>
+                <h2>My personal website</h2>
             </header>
         </>
     );
