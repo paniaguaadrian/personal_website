f6eac835f511b24071c5fc5857f23407c4beb6af
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2022-08-23 21:06:55 +0200

    Delete test paragraph to check pull-request workflow. Working as expected!

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
                     <Navbar />
                     <Header />
                     <About />
-                    <p>Adding an small change to check github actions</p>
                 </>
             }
         />
